c257eb75e55d81f3d9c4335420330643d6d6e4c4
imdad Adelabou <imdadadelabou0@gmail.com>
2021-09-15 13:51:18 +0100

    [feat] Adding login button

--- instagram_flutter_clone/lib/screens/register/register.dart
+++ instagram_flutter_clone/lib/screens/register/register.dart
@@ -16,41 +16,71 @@ class RegisterView extends StatelessWidget {
         child: Scaffold(
           body: Padding(
             padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 20.0),
-                Text(
-                  "Sign up",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: maxWidth * 0.08,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "Poppins",
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20.0),
+                  Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: maxWidth * 0.08,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Poppins",
+                    ),
                   ),
-                ),
-                SizedBox(height: 30.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: returnHeadTitle("Name"),
-                ),
-                const SizedBox(height: space),
-                const CustomTextFormField(),
-                const SizedBox(height: 30.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: returnHeadTitle("Email"),
-                ),
-                const SizedBox(height: space),
-                const CustomTextFormField(),
-                const SizedBox(height: 30.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: returnHeadTitle("Password"),
-                ),
-                const SizedBox(height: space),
-                const CustomTextFormField(),
-              ],
+                  SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: returnHeadTitle("Name"),
+                  ),
+                  const SizedBox(height: space),
+                  const CustomTextFormField(
+                    hintText: "Olivier Cederborg",
+                  ),
+                  const SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: returnHeadTitle("Email"),
+                  ),
+                  const SizedBox(height: space),
+                  const CustomTextFormField(
+                    hintText: "tim@gmail.com",
+                  ),
+                  const SizedBox(height: 30.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: returnHeadTitle("Password"),
+                  ),
+                  const SizedBox(height: space),
+                  const CustomTextFormField(
+                    hintText: "Pick a strong password",
+                  ),
+                  const SizedBox(height: 30.0),
+                  Row(
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Log in",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: "Poppins",
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
