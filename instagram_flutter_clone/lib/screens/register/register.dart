1e6c4e88372c9f4fb7e071ad278da79f31e05d14
imdad Adelabou <imdadadelabou0@gmail.com>
2021-09-15 09:16:23 +0100

    [feat] Add email and Password Textformfield

--- instagram_flutter_clone/lib/screens/register/register.dart
+++ instagram_flutter_clone/lib/screens/register/register.dart
@@ -1,20 +1,26 @@
 import "package:flutter/material.dart";
 import 'package:instagram_flutter_clone/components/base_view.dart';
+import 'package:instagram_flutter_clone/components/custom_textformfield.dart';
 import 'package:instagram_flutter_clone/screens/register/register_model.dart';
+import 'package:instagram_flutter_clone/utils/functions.dart';
 
 class RegisterView extends StatelessWidget {
   static const name = "/register";
+
   @override
   Widget build(BuildContext context) {
     double maxWidth = MediaQuery.of(context).size.width;
+    const double space = 8.0;
     return BaseView<RegisterModel>(
       builder: (BuildContext context, model, child) => SafeArea(
         child: Scaffold(
-          body: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
+          body: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20.0),
+                Text(
                   "Sign up",
                   style: TextStyle(
                     color: Colors.white,
@@ -23,8 +29,29 @@ class RegisterView extends StatelessWidget {
                     fontFamily: "Poppins",
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 30.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: returnHeadTitle("Name"),
+                ),
+                const SizedBox(height: space),
+                const CustomTextFormField(),
+                const SizedBox(height: 30.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: returnHeadTitle("Email"),
+                ),
+                const SizedBox(height: space),
+                const CustomTextFormField(),
+                const SizedBox(height: 30.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: returnHeadTitle("Password"),
+                ),
+                const SizedBox(height: space),
+                const CustomTextFormField(),
+              ],
+            ),
           ),
         ),
       ),
